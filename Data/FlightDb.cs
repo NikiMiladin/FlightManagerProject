b67b406eb55f48ecaf3dce617eec8c15705b115e
ickata1 <ickata1@hotmail.com>
2021-03-22 15:21:35 +0200

    Added the solution file

---
+++ Data/FlightDb.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class FlightDb : DbContext
+    {
+    }
+}
