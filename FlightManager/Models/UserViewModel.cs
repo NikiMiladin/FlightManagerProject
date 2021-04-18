35ccba71c8381d34f54dc0bf7bb114fbc869d563
denib13 <65655906+denib13@users.noreply.github.com>
2021-04-18 11:49:18 +0300

    last minute bug fixes

--- FlightManager/Models/UserViewModel.cs
+++ FlightManager/Models/UserViewModel.cs
@@ -20,11 +20,13 @@ namespace FlightManager.Models
         [MaxLength(10, ErrorMessage = "Max 10 digits")]
         public string EGN { get; set;}
         public string Address { get; set;}
+        [Required]
         [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", ErrorMessage = "Invalid email")]
         public string Email { get; set; }
         [Required]
         [MaxLength(10, ErrorMessage = "Max 10 digits")]
         public string PhoneNumber { get; set; }
+        [Required]
         [DataType(DataType.Password)]
         public string Password { get; set; }     
         public bool IsEmployed {get; set;} 
